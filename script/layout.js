480d68f9837ae6e8aa96a09905db38bf8101c576
Mena Ehab <127118141+commanderYT123@users.noreply.github.com>
2024-09-24 12:57:41 +0300

    Make slider responsive

--- script/layout.js
+++ script/layout.js
@@ -1,10 +1,24 @@
 // slider
 
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
-  spaceBetween: 30,
+  slidesPerView: 1,
+  spaceBetween: 10,
   pagination: {
     clickable: true
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 50
+    }
   }
 });
 
