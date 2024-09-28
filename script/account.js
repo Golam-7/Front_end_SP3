a76d54fb8e6138435159cb7ba997afd5f2fbb512
MennaFathi87 <mnamohamed@gmail.com>
2024-09-28 20:46:51 +0200

    edit in local

--- script/account.js
+++ script/account.js
@@ -1,27 +1,15 @@
-const form = document.getElementById('myform');
-const firstNameInput = document.getElementById('name1');
-const lastNameInput = document.getElementById('name2');
-const emailInput = document.getElementById('emai');
-const addressInput = document.getElementById('adress');
-const passwordInput = document.getElementById('Password');
-const newPasswordInput = document.getElementById('NewP');
-const confirmPasswordInput = document.getElementById('Confirm');
-const savBtn = document.getElementById('save');
-function saveData() {
-  const userData = {
-    name1: firstNameInput.value,
-    name2: lastNameInput.value,
-    email: emailInput.value,
-    address: addressInput.value,
-    password: passwordInput.value,
-    newP: newPasswordInput.value,
-    confirm: confirmPasswordInput.value,
+document.getElementById('save')
+.addEventListener('click', function() {
+  const profileData = {
+      firstname: document.getElementById('name1').value,
+      lastname: document.getElementById('name2').value,
+      email: document.getElementById('email').value,
+      address: document.getElementById('adress').value,
+      password: document.getElementById('Password').value,
+      newPassword: document.getElementById('NewP').value,
+      confirmPassword: document.getElementById('Confirm').value,
   };
-
-  localStorage.setItem('userData', JSON.stringify(userData));
-}
-
-form.addEventListener('button', (event) => {
-  event.preventDefault();
-  saveData();
+  
+  localStorage.setItem('profileData', JSON.stringify(profileData));
+  
 });
